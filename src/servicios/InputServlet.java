b5a3dd9523d0ee727529a774c625b17a2c4707b7
HivanGarches <ivangarcesribas@gmail.com>
2018-03-16 12:03:31 +0100

    Clase autor con un par de correciones.
    Servlet de entrada y salida juntos, con un par de plantillas en
    comentario para poder crear futuros request con facilidad

--- src/servicios/InputServlet.java
+++ src/servicios/InputServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
  * Servlet implementation class InputServlet
  */
@@ -18,27 +20,75 @@ public class InputServlet extends HttpServlet {
 
 
 	
-	 protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	protected void processRequestHandler(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view;
+        String origen = request.getParameter("opcion");
+        //Esto  es la opcion de la pagina de "pruebaboton" , 
+        /*entre los if orige.contentEquals("ValordelaOpcionHiddendlhtml"){metodoProcess}*/
+        if (origen.contentEquals("p1")) {
+        	processBoton(request,response);
+        } else if (origen.contentEquals("p2")) {
+            //Vienes de la pagina 2 y vas a la 3
+        	processLibro(request,response);
+        }
+    }
+            
+    
+	 protected void processBoton (HttpServletRequest request, HttpServletResponse response)
 	            throws ServletException, IOException {
-	        
-	        
+	    	//PASO 3
+	    	//Vas a PruebaBoton
+	    	RequestDispatcher view;
+	    	view = request.getRequestDispatcher("puebaBoton.jsp");
+	    	view.forward(request, response);
 	    }
+	 
+	 protected void processLibro(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        RequestDispatcher view;
+	        //Vas a plantillaLibro
+	       view = request.getRequestDispatcher("plantillaLibro.jsp");
+	       
+	        view.forward(request, response);
+	    }
+	
+	 /*Plantilla para crear un metodo
+      * 
+      * protected void processNombreDelProcesador(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view;
+        
+        view= request.getRequestDispatcher("nombreDelaPagina");
+        
+        view.forward(reques,response);
+        
+        }
+    }*/
+	
+	//metodos doGet y doPost
+         
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		processRequest(request,response);
+		processRequestHandler(request,response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		processRequest(request, response);
+		processRequestHandler(request, response);
 	}
+	
+	
+	    
+	
 
 }
--- src/servicios/Servlet.java
+++ src/servicios/Servlet.java
@@ -1,41 +1,94 @@
 package servicios;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
- * Servlet implementation class Servlet
+ * Servlet implementation class InputServlet
  */
-@WebServlet("/Servlet")
+@WebServlet(urlPatterns = {"/Servlet"})
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
+
+	
+	protected void processRequestHandler(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view;
+        String origen = request.getParameter("opcion");
+        //Esto  es la opcion de la pagina de "pruebaboton" , 
+        /*entre los if orige.contentEquals("ValordelaOpcionHiddendlhtml"){metodoProcess}*/
+        if (origen.contentEquals("p1")) {
+        	processBoton(request,response);
+        } else if (origen.contentEquals("p2")) {
+            //Vienes de la pagina 2 y vas a la 3
+        	processLibro(request,response);
+        }
+    }
+            
+    
+	 protected void processBoton (HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	    	//PASO 3
+	    	//Vas a PruebaBoton
+	    	RequestDispatcher view;
+	    	view = request.getRequestDispatcher("puebaBoton.jsp");
+	    	view.forward(request, response);
+	    }
+	 
+	 protected void processLibro(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        RequestDispatcher view;
+	        //Vas a plantillaLibro
+	       view = request.getRequestDispatcher("plantillaLibro.jsp");
+	       
+	        view.forward(request, response);
+	    }
+	
+	 /*Plantilla para crear un metodo
+      * 
+      * protected void processNombreDelProcesador(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view;
+        
+        view= request.getRequestDispatcher("nombreDelaPagina");
+        
+        view.forward(reques,response);
+        
+        }
+    }*/
+	
+	//metodos doGet y doPost
+         
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		processRequestHandler(request,response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		processRequestHandler(request, response);
 	}
+	
+	
+	    
+	
 
 }
