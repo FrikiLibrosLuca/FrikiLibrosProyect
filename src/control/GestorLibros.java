e600b8645913a05bc49d927a9f75437fd12cf7cd
Lucia <lucia.estradag@gmail.com>
2018-03-20 08:11:00 +0100

    Gestor de Libros new

--- src/control/GestorLibros.java
+++ src/control/GestorLibros.java
@@ -1,41 +1,84 @@
 package control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Libro;
-import servicios.Servicios;
+import servicios.ServiciosLibro;
 
 public class GestorLibros {
 	
-	static Servicios serv=new Servicios();
+	static ServiciosLibro serv=new ServiciosLibro();
 	
 	public HttpServletRequest gestion(HttpServletRequest request, HttpServletResponse response){
 		
 		String opcion=request.getParameter("opcion");
+		Libro lib;
+		List<Libro> listadoLibros;
+		
+		int codError=0;
 		
 		switch(opcion){
-			case "readLibro":
-			
-			case "updateLibro":
-			
-			case "insertLibro":
-			
-			case "deleteLibro":
-			
-			case "readListaLibro":
-			
-		
-		
+			case "buscarLibro":
+				String isbn = request.getParameter("isbn");
+				lib= serv.buscarLibro(isbn);
+				request.setAttribute("libro", lib);
+				break;
+			case "insertarLibro":
+				codError = serv.insertarModificar(request);
+				break;
+			case "eliminarLibro":
+				String isbn1 = request.getParameter("isbn");
+				codError = serv.borrarLibro(isbn1);
+				break;
+			case "leerListaLibro":
+				String columna = request.getParameter("columna");
+				int valorInt;
+				String valorString;
+				
+				if(request.getParameter("columna").isEmpty()){
+					listadoLibros = serv.leerListaLibro();
+					request.setAttribute("libro", listadoLibros);
+				}
+				else{
+					if(this.isNumeric(request.getParameter("valor"))){
+						valorInt =Integer.parseInt(request.getParameter("valor")); 
+						listadoLibros= serv.leerListaLibro(columna, valorInt);
+						request.setAttribute("libro", listadoLibros);
+					}
+					else{
+						valorString = request.getParameter("valor");
+						listadoLibros= serv.leerListaLibro(columna, valorString);
+						request.setAttribute("libro", listadoLibros);
+					}					
+				}
+				break;		
 		}
 		
-		return request;
+		if(codError==-1){
+			request.setAttribute("resultado", "Ha habido un error a la hora de modificar o insertar o eliminar");
+		}
+		else if(codError>=0){
+			request.setAttribute("resultado", "Insertado o modificado o eliminar correctamente");
+		}	
 		
 		
+		
+		return request;		
 	}
 	
 	
-	
+	private static boolean isNumeric(String cadena){
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
+	}
 	
 	public HttpServletRequest mostrarDetallesLibro(HttpServletRequest request){
 		
