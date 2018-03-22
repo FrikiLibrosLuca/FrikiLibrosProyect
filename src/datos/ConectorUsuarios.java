2d4f8b5dcab91c208ebf617fe67ec92d7b71fc7e
Harkle7 <harkle7@gmail.com>
2018-03-22 15:30:40 +0100

    conectores con close de conexion y statement manual

--- src/datos/ConectorUsuarios.java
+++ src/datos/ConectorUsuarios.java
@@ -10,88 +10,78 @@ import model.Usuario;
 
 public class ConectorUsuarios {
 
-	static Connection con = null;
+	private Connection con = null;
 	private Statement st = null;
 	private ResultSet rs = null;
-	
-	public ConectorUsuarios(){
+
+	public ConectorUsuarios() {
 		abrirConnect();
 	}
 
-	
-	public static void abrirConnect() {
+	public void abrirConnect() {
 		try {
 			String driverClassName = "com.mysql.jdbc.Driver";
-			//String driverUrl = "jdbc:mysql://192.168.200.9:8080/frikilibros";
+			// String driverUrl = "jdbc:mysql://192.168.200.9:8080/frikilibros";
 			String driverUrl = "jdbc:mysql://192.168.200.9/frikilibros";
 			String user = "proyecto";
 			String password = "1234";
 			Class.forName(driverClassName);
 			con = DriverManager.getConnection(driverUrl, user, password);
-			
-		} catch (ClassNotFoundException e){
+
+		} catch (ClassNotFoundException e) {
 			System.out.println("No se encuentra el driver");
 		} catch (SQLException e) {
 			System.out.println("Exception SQL: " + e.getMessage());
 			System.out.println("Estado SQL: " + e.getSQLState());
 			System.out.println("Codigo del Error: " + e.getErrorCode());
+		} finally {
+			cerrarConnect();
 		}
 	}
-	
+
 	public void cerrarConnect() {
-		
+
 		try {
-			if(rs != null){
-			rs.close();
+			if (rs != null) {
+				rs.close();
 			}
-			if(st != null){
+			if (st != null) {
 				st.close();
 			}
-			if (con != null){
+			if (con != null) {
 				con.close();
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	public ResultSet leerUsuario(String usuario){
+
+	public ResultSet leerUsuario(String usuario) {
 		String query = "SELECT * FROM usuarios WHERE usuario='" + usuario + "'";
 		try {
 			this.st = con.createStatement();
 			this.rs = st.executeQuery(query);
-			
+
 		} catch (SQLException e) {
 			System.out.println("Exception SQL: " + e.getMessage());
 			System.out.println("Estado SQL: " + e.getSQLState());
 			System.out.println("Codigo del Error: " + e.getErrorCode());
+		} finally {
+			cerrarConnect();
 		}
 		return rs;
 	}
-	
-	public int insertarUsuario(Usuario user){
+
+	public int insertarUsuario(Usuario user) {
 		int privilegios = 0;
 		if (user.isPrivilegios())
 			privilegios = 1;
-			
-		String query = "INSERT INTO usuarios VALUES ('" + user.getUsuario() + "', '" + user.getCorreo() + "', '" + user.getContrasena() +","+privilegios+ ";";
-		
-		int registrosAfectados = -1; 
-			try {
-				this.st = con.createStatement();
-				registrosAfectados = st.executeUpdate(query);
-			} catch (SQLException e) {
-				System.out.println("Exception SQL: " + e.getMessage());
-				System.out.println("Estado SQL: " + e.getSQLState());
-				System.out.println("Codigo del Error: " + e.getErrorCode());
-			}
-			return registrosAfectados;		
-	}
 
-	public int borrarUsuario(String usuario){
-		String query = "DELETE FROM usuarios WHERE usuario='" + usuario + "'";
-		int registrosAfectados = -1; 
+		String query = "INSERT INTO usuarios VALUES ('" + user.getUsuario() + "', '" + user.getCorreo() + "', '"
+				+ user.getContrasena() + "," + privilegios + ";";
+
+		int registrosAfectados = -1;
 		try {
 			this.st = con.createStatement();
 			registrosAfectados = st.executeUpdate(query);
@@ -99,24 +89,45 @@ public class ConectorUsuarios {
 			System.out.println("Exception SQL: " + e.getMessage());
 			System.out.println("Estado SQL: " + e.getSQLState());
 			System.out.println("Codigo del Error: " + e.getErrorCode());
+		} finally {
+			cerrarConnect();
 		}
 		return registrosAfectados;
 	}
-	
-	public int modificarUsuario(Usuario user){
-			
-		String query = "UPDATE usuarios SET correo='" + user.getCorreo() + "', contrasena='" + user.getContrasena() + ";";
-		
-		int registrosAfectados = -1; 
-			try {
-				this.st = con.createStatement();
-				registrosAfectados = st.executeUpdate(query);
-			} catch (SQLException e) {
-				System.out.println("Exception SQL: " + e.getMessage());
-				System.out.println("Estado SQL: " + e.getSQLState());
-				System.out.println("Codigo del Error: " + e.getErrorCode());
-			}
-			return registrosAfectados;		
+
+	public int borrarUsuario(String usuario) {
+		String query = "DELETE FROM usuarios WHERE usuario='" + usuario + "'";
+		int registrosAfectados = -1;
+		try {
+			this.st = con.createStatement();
+			registrosAfectados = st.executeUpdate(query);
+		} catch (SQLException e) {
+			System.out.println("Exception SQL: " + e.getMessage());
+			System.out.println("Estado SQL: " + e.getSQLState());
+			System.out.println("Codigo del Error: " + e.getErrorCode());
+		} finally {
+			cerrarConnect();
+		}
+		return registrosAfectados;
 	}
-	
+
+	public int modificarUsuario(Usuario user) {
+
+		String query = "UPDATE usuarios SET correo='" + user.getCorreo() + "', contrasena='" + user.getContrasena()
+				+ ";";
+
+		int registrosAfectados = -1;
+		try {
+			this.st = con.createStatement();
+			registrosAfectados = st.executeUpdate(query);
+		} catch (SQLException e) {
+			System.out.println("Exception SQL: " + e.getMessage());
+			System.out.println("Estado SQL: " + e.getSQLState());
+			System.out.println("Codigo del Error: " + e.getErrorCode());
+		} finally {
+			cerrarConnect();
+		}
+		return registrosAfectados;
+	}
+
 }
